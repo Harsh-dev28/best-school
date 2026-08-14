e488dd6c2a5c9b4eae632b4ac719ee9ed5a394a7
Harsh-dev28 <harsh28kushwah@gmail.com>
2026-08-14 19:36:32 +0530

    j

--- src/app/component/home-sections/About.jsx
+++ src/app/component/home-sections/About.jsx
@@ -3,69 +3,69 @@ import React from 'react'
 
 function About() {
     return (
-        <section className='text-center pt-40'>
+        <section className='text-center pt-40 px-4'>
             <div className=''>
-                <span className='a text-[42px]'>Scientific Approach of Finding Best School</span>
+                <span className='a text-[28px] sm:text-[34px] md:text-[38px] lg:text-[42px]'>Scientific Approach of Finding Best School</span>
             </div>
 
             <div className="text-grey-150">
-                <span className='a1 text-[21px]'>Experience the Power of Data Analysis</span>
+                <span className='a1 text-[17px] sm:text-[19px] md:text-[21px]'>Experience the Power of Data Analysis</span>
             </div>
 
-            <div className='flex p-5'>
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Mumbai.png' className='border-10 border-gray-500' />
+            <div className='flex flex-col md:flex-row justify-center items-center p-5 gap-5'>
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Mumbai.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
 
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Chennai.png' className='border-10 border-gray-500' />
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Chennai.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
 
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Bangalore.png' className='border-10 border-gray-500' />
-                </div>
-            </div>
-
-            <div className='flex p-5'>
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Delhi-NCR.png' className='border-10 border-gray-500' />
-                </div>
-
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Hyderabad.png' className='border-10 border-gray-500' />
-                </div>
-
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Kolkata.png' className='border-10 border-gray-500' />
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Bangalore.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
             </div>
 
-            <div className='flex p-5'>
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Pune.png' className='border-10 border-gray-500' />
+            <div className='flex flex-col md:flex-row justify-center items-center p-5 gap-5'>
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Delhi-NCR.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
 
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Lucknow.png' className='border-10 border-gray-500' />
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Hyderabad.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
 
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Lucknow.png' className='border-10 border-gray-500' />
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Kolkata.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
+                </div>
+            </div>
+
+            <div className='flex flex-col md:flex-row justify-center items-center p-5 gap-5'>
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Pune.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
+                </div>
+
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Lucknow.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
+                </div>
+
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Lucknow.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
             </div>
 
 
-            <div className='flex p-5'>
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Jaipur.png' className='border-10 border-gray-500' />
+            <div className='flex flex-col md:flex-row justify-center items-center p-5 gap-5'>
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Jaipur.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
 
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Dehradun.png' className='border-10 border-gray-500' />
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Dehradun.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
 
-                <div className='pr-5'>
-                    <img src='https://bestschool.net.in//images/top_schools_of_Surat.png' className='border-10 border-gray-500' />
+                <div className='pr-0 md:pr-5 w-full md:w-auto'>
+                    <img src='https://bestschool.net.in//images/top_schools_of_Surat.png' className='border-10 border-gray-500 w-full max-w-[400px] mx-auto' />
                 </div>
             </div>
 
@@ -73,12 +73,12 @@ function About() {
                 <button className=''>Other Cities...</button>
             </div>
 
-            <div className='pt-30 pb-20 bg-gray-100 text-gray-500'>
-                <span className='text-[25px] italic'>"We recently moved to Delhi and it was all confusing about finding school every one had deferent</span><br></br>
+            <div className='pt-20 sm:pt-30 pb-20 bg-gray-100 text-gray-500 px-4'>
+                <span className='text-[18px] sm:text-[21px] md:text-[25px] italic'>"We recently moved to Delhi and it was all confusing about finding school every one had deferent</span><br></br>
 
-                <span className='italic text-[25px]'>choice but your website made it so simple, Thank You."</span><br></br>
+                <span className='italic text-[18px] sm:text-[21px] md:text-[25px]'>choice but your website made it so simple, Thank You."</span><br></br>
 
-                <span className='italic text-[20px] pt-30'>- A BestSchool Parent</span>
+                <span className='italic text-[16px] sm:text-[18px] md:text-[20px] pt-20 sm:pt-30'>- A BestSchool Parent</span>
             </div>
 
             <div className='p-10'></div>
@@ -90,3 +90,4 @@ function About() {
 }
 
 export default About
+
--- src/app/component/home-sections/HeroSection.jsx
+++ src/app/component/home-sections/HeroSection.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 function Hero() {
     return (
-        <section className='min-h-screen bg-[url(https://bestschool.net.in/templates/purity_iii/images/bg.jpg)] bg-cover bg-center text-white text-center pt-60'>
-            <div className='text-[42px]'>
+        <section className='min-h-screen bg-[url(https://bestschool.net.in/templates/purity_iii/images/bg.jpg)] bg-cover bg-center text-white text-center pt-40 sm:pt-48 md:pt-56 lg:pt-60 px-4'>
+            <div className='text-[28px] sm:text-[34px] md:text-[38px] lg:text-[42px]'>
                 <span className='b'>Guide your child to a better future</span>
             </div>
 
             <div className='text-shadow-[1px 1px #222222]'>
-                <h4 className='b text-[30px]'>Find the best school</h4>
+                <h4 className='b text-[20px] sm:text-[24px] md:text-[27px] lg:text-[30px]'>Find the best school</h4>
             </div>
 
 
-            <div className='c'>
-                <input type='text' placeholder=' Start typing city, zip, address or landmark' className='pt-1 w-[600px] h-[38px]' />
+            <div className='c px-2'>
+                <input type='text' placeholder=' Start typing city, zip, address or landmark' className='pt-1 w-full max-w-[600px] h-[38px]' />
             </div>
 
         </section>
